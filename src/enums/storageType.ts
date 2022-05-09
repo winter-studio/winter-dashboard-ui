b142bb27577c6e9c808bc02dda882334a4737415
Kyun <me@iamkyun.com>
2022-05-09 11:30:46 +0800

    remove persistent menus

--- src/enums/storageType.ts
+++ src/enums/storageType.ts
@@ -1,7 +1,6 @@
 enum StorageType {
   ACCESS_TOKEN = 'ACCESS_TOKEN', // 用户token
   CURRENT_USER = 'CURRENT_USER', // 当前用户信息
-  CURRENT_USER_MENUS = 'CURRENT_USER_MENUS', // 当前用户菜单
   TABS_ROUTES = 'TABS_ROUTES' // 切换标签页
 }
 
