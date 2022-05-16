35c7df6a66a11a6335d737269f7bce5eeb91db14
Kyun <me@iamkyun.com>
2022-05-16 11:32:02 +0800

    remove used files

--- src/components/form/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BasicForm } from './BasicForm.vue'
-export { useForm } from './hooks/useForm'
-export * from './types/form'
-export * from './types/index'
--- src/components/modal/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as basicModal } from './BasicModal.vue'
-export { useModal } from './hooks/useModal'
-export * from './type'
