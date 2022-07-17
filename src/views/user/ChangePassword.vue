8b713acaa99af2c4c8b0552dd894f41ad5e58949
Kyun <me@iamkyun.com>
2022-07-17 23:18:32 +0800

    user settings

--- src/views/user/ChangePassword.vue
+++ src/views/user/ChangePassword.vue
@@ -3,84 +3,68 @@
     <template #header>
       <n-button attr-type="button" type="primary" @click="save"> 保存</n-button>
     </template>
-    <n-spin :show="formLoading">
-      <template #description> 加载中...</template>
-      <n-form
-        ref="formRef"
-        :model="userForm"
-        :rules="userFormRules"
-        label-placement="left"
-        label-width="auto"
-        require-mark-placement="right-hanging"
-        class="w-1/3"
-      >
-        <n-form-item label="头像" path="avatar">
-          <div class="flex flex-col">
-            <n-avatar
-              class="bg-gray-300 bg-opacity-60 mb-2"
-              circle
-              :size="84"
-              :src="userForm.avatar"
-            />
-            <n-button quaternary type="primary" @click="handleClickUpload"> 更换头像</n-button>
-            <input id="fileUploader" type="file" style="display: none" @change="afterUploadFile" />
-          </div>
-        </n-form-item>
-        <n-form-item label="昵称" path="nickname">
-          <n-input v-model:value="userForm.nickname" />
-        </n-form-item>
-      </n-form>
-    </n-spin>
+    <n-form
+      ref="formRef"
+      :model="form"
+      :rules="rules"
+      label-placement="left"
+      label-width="auto"
+      require-mark-placement="right-hanging"
+      class="w-1/3"
+    >
+      <n-form-item label="当前密码" path="oldPassword">
+        <n-input v-model:value="form.oldPassword" placeholder="请输入当前密码" />
+      </n-form-item>
+      <n-form-item label="新密码" path="newPassword">
+        <n-input v-model:value="form.newPassword" placeholder="请输入新密码密码" />
+      </n-form-item>
+      <n-form-item label="新密码确认" path="confirmPassword">
+        <n-input v-model:value="form.confirmPassword" placeholder="请再输入一次新密码" />
+      </n-form-item>
+    </n-form>
   </n-card>
 </template>
 
 <script setup lang="ts">
-import { FormInst, useMessage } from 'naive-ui'
-import { onMounted, ref } from 'vue'
-import { getUser, updateUserInfo, uploadAvatar } from '@/api/user/user'
+import { FormInst, useMessage, FormRules, FormItemRule } from 'naive-ui'
+import { ref } from 'vue'
+import { changePassword } from '@/api/user/user'
 import { useUserStore } from '@/store/modules/user'
-import { FormRules } from 'naive-ui/es/form/src/interface'
-import { UserProfile } from '@/types/modules/user'
+import { UserPassword } from '@/types/modules/user'
+import { isEqual } from 'lodash-es'
 
 const userStore = useUserStore()
 const message = useMessage()
-const formLoading = ref(false)
 const formRef = ref<FormInst | null>(null)
 
-const userForm = ref<UserProfile>({
-  avatar: '',
-  nickname: ''
-})
+interface UserPasswordConfirm extends UserPassword {
+  confirmPassword: string
+}
 
-const userFormRules: FormRules = {
-  nickname: [
-    { required: true, message: '请输入昵称', trigger: 'blur' },
-    { max: 20, message: '昵称只能20个字符内', trigger: 'blur' }
+const form = ref<UserPasswordConfirm | undefined>(undefined)
+
+const rules: FormRules = {
+  oldPassword: [{ required: true, message: '请输入当前密码', trigger: 'blur' }],
+  newPassword: [
+    {
+      message: '密码只能由6-32位字母、数字、下划线组成',
+      trigger: 'blur',
+      pattern: /^[a-zA-Z0-9_]{6,32}$/
+    }
+  ],
+  confirmPassword: [
+    {
+      required: true,
+      trigger: 'blur',
+      validator(_: FormItemRule, value: string) {
+        if (isEqual(value, form.value?.newPassword)) {
+          return new Error('两次输入的密码不一致')
+        }
+
+        return true
+      }
+    }
   ]
-}
-
-onMounted(() => {
-  getUser(userStore.userInfo!.userId).then((res) => {
-    userForm.value.avatar = res.data!.avatar
-    userForm.value.nickname = res.data!.nickname
-  })
-})
-
-function handleClickUpload() {
-  document.getElementById('fileUploader')!.click()
-}
-
-function afterUploadFile() {
-  const ele = document.getElementById('fileUploader') as HTMLInputElement
-  if (ele) {
-    const file = ele.files![0]
-    const formData = new FormData()
-    formData.append('avatar', file)
-    uploadAvatar(formData).then((res) => {
-      userForm.value.avatar = res.data!
-      ele.value = ''
-    })
-  }
 }
 
 function save() {
@@ -89,9 +73,11 @@ function save() {
       message.error('请完整填写表单')
       return
     }
-  })
-  updateUserInfo(userForm.value).then(() => {
-    message.success('修改成功')
+
+    changePassword(form.value!).then(() => {
+      message.success('修改成功')
+    })
+    userStore.updateUserInfo()
   })
 }
 </script>
