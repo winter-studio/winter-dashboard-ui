99eeff95d3968b6ed6488299f560154c5462df88
Kyun <me@iamkyun.com>
2022-07-22 18:00:16 +0800

    dict module

--- src/store/modules/dict.ts
+++ src/store/modules/dict.ts
@@ -1,15 +1,18 @@
 import { defineStore } from 'pinia'
 import { FormSelectOption } from '@/types/component/form'
 import { getRoleOptions } from '@/api/user/role'
+import { DictItem } from '@/types/modules/dict'
 
 export interface DictState {
   roleOptions?: FormSelectOption[]
+  dicts: Map<string, DictItem[]>
 }
 
 export const useDictStore = defineStore({
   id: 'dict',
   state: (): DictState => ({
-    roleOptions: undefined
+    roleOptions: undefined,
+    dicts: new Map<string, DictItem[]>()
   }),
   getters: {},
   actions: {
@@ -19,6 +22,12 @@ export const useDictStore = defineStore({
           if (this.roleOptions) return
           getRoleOptions().then((res) => (this.roleOptions = res.data!))
           break
+      }
+    },
+    getDict(code: string) {
+      if (this.dicts.has(code)) {
+        //TODO
+        return this.dicts.get(code)
       }
     }
   }
