f523c5c87adfb721bc0123edde13d5ab9a171d84
Kyun <me@iamkyun.com>
2022-07-27 10:41:11 +0800

    better animation

--- src/components/table/WinterTable.vue
+++ src/components/table/WinterTable.vue
@@ -7,8 +7,15 @@
           <n-button type="default" @click="reset"> 重置 </n-button>
         </div>
       </template>
-      <n-form ref="formRef" :model="searchForm" label-placement="left" :label-width="80">
-        <n-grid :cols="24" :x-gap="24" :collapsed-rows="2" :collapsed="collapsed">
+      <n-form
+        ref="formRef"
+        :model="searchForm"
+        label-placement="left"
+        :label-width="80"
+        class="search-grid"
+        :style="{ height: searchGridHeight }"
+      >
+        <n-grid id="height-probe" :cols="24" :x-gap="24" :collapsed-rows="1" :collapsed="collapsed">
           <n-form-item-gi
             v-for="(item, index) in searchItems"
             :key="index"
@@ -18,26 +25,26 @@
           >
             <n-input v-model:value="searchForm[item.path]" :placeholder="item.placeholder" />
           </n-form-item-gi>
-          <n-gi :span="24" suffix #="{ overflow }">
-            <n-divider v-if="overflow" class="more-condition">
-              <n-button text @click="collapsed = false">
-                <template #icon>
-                  <n-icon><angle-double-down /></n-icon>
-                </template>
-                更多条件
-              </n-button>
-            </n-divider>
-            <n-divider v-if="!collapsed" class="more-condition">
-              <n-button text @click="collapsed = true">
-                <template #icon>
-                  <n-icon><angle-double-up /></n-icon>
-                </template>
-                收起条件
-              </n-button>
-            </n-divider>
-          </n-gi>
         </n-grid>
       </n-form>
+      <div v-if="searchItems.length > 3">
+        <n-divider v-if="collapsed" class="more-condition">
+          <n-button text @click="toggleCollapsed">
+            <template #icon>
+              <n-icon><angle-double-down /></n-icon>
+            </template>
+            更多条件
+          </n-button>
+        </n-divider>
+        <n-divider v-else class="more-condition">
+          <n-button text @click="toggleCollapsed">
+            <template #icon>
+              <n-icon><angle-double-up /></n-icon>
+            </template>
+            收起条件
+          </n-button>
+        </n-divider>
+      </div>
     </n-card>
     <div class="mt-3 mb-1">
       <slot name="table-header"></slot>
@@ -52,7 +59,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { nextTick, onMounted, ref } from 'vue'
 import { AngleDoubleDown, AngleDoubleUp } from '@vicons/fa'
 import { DataTableColumns, NButton } from 'naive-ui'
 import { RowData } from 'naive-ui/es/data-table/src/interface'
@@ -83,6 +90,13 @@ const emits = defineEmits<{
 
 const searchForm = ref<any>({})
 const collapsed = ref(true)
+const searchGridHeight = ref('100%')
+
+onMounted(() => {
+  const probe = document.getElementById('height-probe')
+  const offsetHeight = probe!.offsetHeight
+  searchGridHeight.value = `${offsetHeight}px`
+})
 
 const pagination = ref({
   page: props.page,
@@ -118,9 +132,23 @@ function search() {
     ...clone(searchForm.value)
   })
 }
+
+async function toggleCollapsed() {
+  collapsed.value = !collapsed.value
+  await nextTick()
+  const probe = document.getElementById('height-probe')
+  const offsetHeight = probe!.offsetHeight
+  searchGridHeight.value = `${offsetHeight}px`
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.search-grid {
+  transition: height 0.2s linear;
+  height: v-bind(searchGridHeight);
+  overflow: hidden;
+}
+
 .more-condition {
   margin: 0;
 }
